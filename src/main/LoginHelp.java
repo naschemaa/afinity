896c898b81e5f3d7cf089bca5a4fbf0f00493fcf
naschemaa <96695646+naschemaa@users.noreply.github.com>
2023-08-21 19:08:30 +0200

    Update LoginHelp.java

--- src/main/LoginHelp.java
+++ src/main/LoginHelp.java
@@ -7,7 +7,7 @@ import java.awt.Window;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-import javax.swing.ImageIcon;
+// import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPasswordField;
